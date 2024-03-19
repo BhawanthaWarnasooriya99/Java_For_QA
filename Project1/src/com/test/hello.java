94581dedf4063a70cc7b17e4713aeef9ed3f6473
BhawanthaWarnasooriya99 <bhawantha1002@gmail.com>
2024-03-20 02:11:48 +0530

    second commit of other methods in hello class

--- Project1/src/com/test/hello.java
+++ Project1/src/com/test/hello.java
@@ -5,5 +5,24 @@ public class hello {
 	public static void main(String[] args) {
 
 		System.out.println("Hello pasindu");
+		addVariablesAndPrint(5,10);
+		addVariablesAndPrint(6,10);
+		printName("warnasooriya");
 	}
+	
+	//a = 10
+	//b = 5
+	//I need a method to add two variables and print it
+	
+	public static void addVariablesAndPrint(int a, int b) {
+		
+		int tot = a + b;
+		
+		System.out.println("tot = " +tot);
+	}
+	
+	public static void printName(String name) {
+		System.out.println(name);
+	}
+	
 }
